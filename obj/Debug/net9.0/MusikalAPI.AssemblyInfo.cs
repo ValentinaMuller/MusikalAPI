31f1269da0d5e6807b0795e242b4257dfbc503e3
Valentina Muller <valentinanicolemuller@gmail.com>
2025-12-15 20:08:47 -0300

    Ultimos cambios

--- obj/Debug/net9.0/MusikalAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/MusikalAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusikalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7f25c7376a9210e2800c6616a8b6dae3a0a9908")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29dc056e8a959570ec1d7b9d122785f640e999d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusikalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusikalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
